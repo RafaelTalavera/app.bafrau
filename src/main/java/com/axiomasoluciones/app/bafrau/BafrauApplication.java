c92193699f3c1b66afdd0cc26b61726a34fe98bb
Rafael Fernando Talavera <125708112+RafaelTalavera@users.noreply.github.com>
2025-06-07 08:29:19 -0300

    Update BafrauApplication.java
    Enable

--- src/main/java/com/axiomasoluciones/app/bafrau/BafrauApplication.java
+++ src/main/java/com/axiomasoluciones/app/bafrau/BafrauApplication.java
@@ -3,10 +3,15 @@ package com.axiomasoluciones.app.bafrau;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class BafrauApplication {
 
 	public static void main(String[] args) {
@@ -16,5 +21,10 @@ public class BafrauApplication {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional.ofNullable("system");
 	}
 }
